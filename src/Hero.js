2268180e98f475af8bbc869c48a728f5e8df8f91
patd <pdoran@gmail.com>
2021-02-07 10:45:24 -0500

    WIP hero movement

--- src/Hero.js
+++ src/Hero.js
@@ -6,15 +6,64 @@ class Hero extends Component {
     this.state = {
       heroFacing: "down", // Facing Up, Down, Left, Right
       heroMoving: "stopped", // movingLeft, movingright,movingUp,movingDown
-
     };
-    // this.showModal = this.showModal.bind(this);
-    // this.hideModal = this.hideModal.bind(this);
   }
 
 
-  componentDidUpdate() {
-    // console.log(this.props.heroDirection)
+  setHeroMoveState(prevProps){
+    if (prevProps.heroDirection !== this.props.heroDirection) {
+      console.log('component diff')
+      // console.log("--------------------")
+      // console.log("Key press is " + this.props.heroDirection)
+      // console.log("Hero is facing:" + this.state.heroFacing)
+      // console.log("Hero is moving:" + this.state.heroMoving)
+      // console.log("--------------------")
+
+      // if(this.state.heroMoving === "stopped" && this.props.heroDirection === 'ArrowUp'){
+      //   this.setState({heroFacing:"up",heroMoving:"moving"});
+      // } else if(this.state.heroMoving === "moving" && this.props.heroDirection === 'ArrowUp'){
+      //   this.setState({heroFacing:"up",heroMoving:"stopped"});
+      // } else{
+
+      // }
+
+
+    } else if(prevProps.heroDirection === this.props.heroDirection){
+      console.log('component same')
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    // update component
+    this.setHeroMoveState(prevProps)
+    //           // // Move or not
+    //           // if(this.state.heroMoving === "stopped"){
+    //           //   this.setState({heroMoving:"moving"})
+    //           // } else{
+    //           //   this.setState({heroMoving:"stopped"})
+    //           // }
+
+    // if (prevProps.heroDirection !== this.props.heroDirection) {
+      
+    //   // change direction 
+    //   if(this.props.heroDirection === "ArrowUp"){
+    //     this.setState({heroFacing:"up"})
+    //     // if(this.state.heroMoving === "stopped"){
+    //     //   this.setState({heroMoving:"moving"})
+    //     // } else{
+    //     //   this.setState({heroMoving:"stopped"})
+    //     // }
+        
+        
+    //   } else if(this.props.heroDirection === "ArrowDown"){
+    //     this.setState({heroFacing:"down"})
+    //   } else if(this.props.heroDirection === "ArrowLeft"){
+    //     this.setState({heroFacing:"left"})
+    //   } else {
+    //     this.setState({heroFacing:"right"})
+    //   }
+    // }
+
   }
 
   render(props) {
