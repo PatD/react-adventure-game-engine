22615feb37eae8512953794f6db98c2e2acaaa11
patd <pdoran@gmail.com>
2021-04-11 21:00:43 -0400

    WIP adding 'look' to text parser

--- src/TextInputParser.js
+++ src/TextInputParser.js
@@ -18,11 +18,21 @@ export default class TextInputParser extends Component {
   handleTextParsing(){
     if(this.props.submittedText.length>0 && this.state.prevSubmittedText !== this.props.submittedText){
       console.log("User submitted this text: " + this.props.submittedText)
-      
-      const wordArray = this.props.submittedText.split(/\s+/);
+  
+      // Lowercase everything
+      const makeLowerCase = this.props.submittedText.toLowerCase()
+
+      // Take each word the user inputs and add to an array
+      const makeWordArray = makeLowerCase.split(/\s+/);
+
+      // Remove unneeded words and chars
+      const garbageWords = [" ","","the","at","to","of","a","for","all","some","i","next","with","around","through","on","in"];
+
+      // An array of the essential - hopefully just a verb and a noun
+      const textForParsing = makeWordArray.filter(item => !garbageWords.includes(item))      
 
       // Breaks user input into an array and puts in state
-      this.setState({wordArray});
+      this.setState({textForParsing});
 
       // In this component, record this as the previous state, so we can check it next time.
       this.setState({prevSubmittedText:this.props.submittedText});
@@ -32,12 +42,12 @@ export default class TextInputParser extends Component {
       // Assume these verbs are in most games.  Additional support provided from customVerbs in gamedata.json
       //  "help","push","pull","eat","turn","inventory","look","open","examine","close","inventory","save","restart","restore","inspect","get","pick","drop","talk","read"
 
-      if(wordArray.includes('look')){
-        this.verbLook(wordArray)
-      } else if(wordArray.includes('help')){
-          this.help(wordArray)
-      } else if (wordArray.includes('push')){
-          this.push(wordArray)
+      if(textForParsing.includes('look')){
+        this.verbLook(textForParsing)
+      } else if(textForParsing.includes('help')){
+          this.help(textForParsing)
+      } else if (textForParsing.includes('push')){
+          this.push(textForParsing)
       } else{
           return console.log('wut')
       }
@@ -45,17 +55,29 @@ export default class TextInputParser extends Component {
     }
   }
 
-  verbLook = (wordArray) =>{
-    console.log(wordArray)
-  
+  verbLook = (textForParsing) =>{
+    console.log(JSON.stringify(textForParsing))
+
+    // If just "look" (or 'look room' is typed, 
+    // then read the room Description from this.roomCurrentDescription
+    if(JSON.stringify(textForParsing) === '["look"]' || JSON.stringify(textForParsing) === '["look","room"]'){
+
+      if(this.props.roomCurrentDescription === "" || this.props.roomCurrentDescription === " " || this.props.roomCurrentDescription === null || this.props.roomCurrentDescription === "undefined"){
+        console.log("You don't see much")
+      } else{
+        console.log(this.props.roomCurrentDescription)
+      }
+
+    }
+    
+
     // What inventory items can be looked at?
       // Loop through this.props.inventory and read 'Description' if 'owned' is true
   
     // What displayobjects in current room can be looked at?
       // Loop through this.props.roomCurrentObjects and if it has a Description that isn't "" or null
 
-    // If just "look" is typed, read the room Description from this.roomCurrentDescription
-  
+    
   
   
   }
